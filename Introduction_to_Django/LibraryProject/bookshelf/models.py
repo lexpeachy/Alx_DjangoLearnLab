00d9133199233a49df461375e2b09f6fab0a5ce9
evans kirimi <evanskirimi483@gmail.com>
2024-11-04 00:17:21 +0300

    modified

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+    publication_year = models.IntegerField(null=True, blank=True)
     
     def __str__(self):
         return self.title
