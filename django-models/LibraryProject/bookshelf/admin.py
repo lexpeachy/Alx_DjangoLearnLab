0158704285bb3954e0d2ef74db8fa2504a82581a
evans kirimi <evanskirimi483@gmail.com>
2024-11-09 23:53:39 +0300

    modified

--- django-models/LibraryProject/bookshelf/admin.py
+++ django-models/LibraryProject/bookshelf/admin.py
@@ -4,7 +4,7 @@ from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")
-    search_fields =('title' 'author')
+    search_fields =['title' 'author']
     list_filter =()
 
 
