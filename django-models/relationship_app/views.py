3b324916e18ce3d541d02e2043505e6eb67632a6
evans kirimi <evanskirimi483@gmail.com>
2024-11-10 06:44:46 +0300

    commit

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Book
 
 def list_books(request):
-    books=Book.object.all()
+    books=Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 from .forms import BookForm
