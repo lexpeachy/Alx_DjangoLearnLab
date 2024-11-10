2c94fe5081999287279d49f6df8be5b2c0cc27f9
evans kirimi <evanskirimi483@gmail.com>
2024-11-10 06:47:28 +0300

    commit

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from .views import list_books
 urlpatterns = [
     path('books/', list_books, name='list_books'),  # URL for function-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # URL for class-based view
