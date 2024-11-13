8f19a8172869605bcd8a07cad42bcfc58494e1a0
evans kirimi <evanskirimi483@gmail.com>
2024-11-14 00:41:32 +0300

    Implement Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -43,3 +43,17 @@ class UserProfile(models.Model):
     
     def __str__(self):
         return self.title
+
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
