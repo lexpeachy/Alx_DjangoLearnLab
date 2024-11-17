40e5371efb6b3cc6a840cb3902e2fa8c3a0b16b1
evans kirimi <evanskirimi483@gmail.com>
2024-11-17 21:35:37 +0300

    Managing Permissions and Groups in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,6 +4,23 @@ from django.shortcuts import render
 from .models import Book
 from .forms import ExampleForm
 
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404
+from .models import Book
+
+@permission_required('yourapp.can_view', raise_exception=True)
+def view_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'book_detail.html', {'book': book})
+
+@permission_required('yourapp.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    # Logic to edit the book
+    return render(request, 'edit_book.html', {'book': book})
+
+
+
 def search_books(request):
     query = request.GET.get('q')
     books = Book.objects.filter(title__icontains=query)
