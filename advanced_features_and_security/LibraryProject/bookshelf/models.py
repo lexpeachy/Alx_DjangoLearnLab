c7d57b64c10fa042479989b842352abbc5a1df3a
evans kirimi <evanskirimi483@gmail.com>
2024-11-17 14:34:30 +0300

    Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -10,26 +10,26 @@ class Book(models.Model):
         return self.title
 from django.contrib.auth.models import abstractuser, BaseUserManager
 
-class customuser(abstractuser):
+class CustomUser(AbstractUser):
     date_of_birth =models.Datefield(null=True blank=True)
     profile_photo =models.Imagefield(upload_to ='profile_photo/')
 
     objects = CustomUserManager()
-class customusermanager(BaseUserManager):
+from django.contrib.auth.models import BaseUserManager
 
+class CustomUserManager(BaseUserManager):
     def create_user(self, email, date_of_birth, password=None, **extra_fields):
         if not email:
-            raise valueError('The email field must be set')
-        if not date_of_birth:
-            raise valueError('The field is required')
-        Email =self.normalize_email(email)
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
         user = self.model(email=email, date_of_birth=date_of_birth, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-
         return user
-    def create_superuser(self, date_of_birth, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True) 
+
+    def create_superuser(self, email, date_of_birth, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+
         return self.create_user(email, date_of_birth, password, **extra_fields)
 
