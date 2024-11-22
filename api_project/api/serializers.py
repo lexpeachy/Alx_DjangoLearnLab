ec228bbe10c0e94807e5908eab28d4e78c874793
evans kirimi <evanskirimi483@gmail.com>
2024-11-23 02:24:45 +0300

    First API Endpoint with Django REST Framework

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -1,7 +1,7 @@
-from rest_framework import serializer
+from rest_framework import serializers
 from .model import Book
 
-class Bookserializer(rest_framework.serializers.modelserializer):
+class Bookserializer(rest_framework.serializers.ModelSerializer):
     class meta():
         model = Book
         fields "__all__"
