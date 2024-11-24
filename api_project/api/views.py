be39bb5d56e669f5eac01c0182112ade40164068
evans kirimi <evanskirimi483@gmail.com>
2024-11-24 12:48:12 +0300

    commit

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -6,3 +6,6 @@ from rest_framework import viewsets
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()  # gets all books from the database
     serializer_class = BookSerializer # use bookserializer for serializatin
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()  # Get all books from the database
+    serializer_class = BookSerializer  # Use the BookSerializer for serialization
