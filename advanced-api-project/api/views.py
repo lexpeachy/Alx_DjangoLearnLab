ed170b9fd9003a9a7b7b333f08632b92ac9787c8
evans kirimi <evanskirimi483@gmail.com>
2024-12-01 16:18:31 +0300

    commits

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -11,7 +11,7 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classses = [AllowAny]# Allow unauthenticated users to view the list
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['title', 'author__name', 'publication_year']
     search_fields = ['title', 'author__name']
     ordering_fields = ['title', 'publication_year']
