600aebd3f4db0c4e1dc812366510aa02f8f1487c
evans kirimi <evanskirimi483@gmail.com>
2024-12-14 01:35:52 +0300

    commit

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -9,7 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()(write_only=True)
 
     class Meta:
         model = get_user_model()
