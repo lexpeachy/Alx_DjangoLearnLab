866d7f149f8fbccb31d67fd337dc861d551ec4e6
evans kirimi <evanskirimi483@gmail.com>
2024-12-15 22:41:03 +0300

    modified

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -67,6 +67,6 @@ class FeedView(APIView):
 
     def get(self, request):
         following_users = request.user.following.all()
-        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+        posts = Post.objects.filter(author__in=following_users).order_by
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data, status=200)
